3bdff826997060f3d7546823d14a511f0fc5427b
Mikiyas Woubshet <mikiyaswoubshet@gmail.com>
2025-08-30 01:50:38 -0700

    Calculator design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Counter from './components/Counter'
 import Todo from './components/todo'
+import Calculator from './components/Calculator'
 
 const App = () => {
   return (
     <div>
       <Counter/>
       <Todo/>
+      <Calculator/>
     </div>
   )
 }
